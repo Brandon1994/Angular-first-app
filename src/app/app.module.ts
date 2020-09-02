9388480fd65cd9adb772f12976d1befbfb4421ff
Brandon1994 <brandon2_@hotmail.com>
2020-09-01 22:43:16 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MovieItemComponent } from './movie-item/movie-item.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieItemComponent,
+    MovieListComponent
   ],
   imports: [
     BrowserModule
